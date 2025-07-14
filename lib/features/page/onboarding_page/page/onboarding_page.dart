9605ab935ed9d4ce088fa5e40a27cab6a6702ec8
MostafaSensei106 <wwwmostafagmer100@gmail.com>
2025-07-14 19:38:34 +0300

    feat(onboarding): Implement page indicator and navigation

--- lib/features/page/onboarding_page/page/onboarding_page.dart
+++ lib/features/page/onboarding_page/page/onboarding_page.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'package:takyeebbak/core/routing/routes.dart';
+import 'package:takyeebbak/core/widgets/button_components/elevated_button_components/elevated_button_icon_component.dart';
+import 'package:takyeebbak/core/widgets/button_components/icon_button_components/icon_button_component.dart';
+import 'package:takyeebbak/core/widgets/button_components/icon_button_components/icon_button_filledtonal_component.dart';
+import 'package:takyeebbak/core/widgets/lottie_component/lottie_component.dart';
 
 class OnboardingPage extends StatefulWidget {
   const OnboardingPage({super.key});
@@ -43,13 +48,60 @@ class _OnboardingPageState extends State<OnboardingPage> {
       return const Scaffold(body: Center(child: CircularProgressIndicator()));
     }
     return Scaffold(
-      body: PageView(
-        controller: _pageController,
-        children: const [
-          Center(child: Text('Page 1')),
-          Center(child: Text('Page 2')),
-          Center(child: Text('Page 3')),
-        ],
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: _navigateToHome,
+                    child: const Text('Skip'),
+                  ),
+                ],
+              ),
+              // PageView(
+              //   controller: _pageController,
+              //   children: const [
+              //     LottieComponent(lottiePath: 'assets/lottie/1.json', text: 'Welcome to Takyeebbak!')
+              //   ],
+              // ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SmoothPageIndicator(
+                    controller: _pageController,
+                    count: 4,
+                    effect: ExpandingDotsEffect(
+                      // dotWidth: AppC.indicatorDotSize,
+                      // dotHeight: SenseiConst.indicatorDotSize,
+                      dotColor: Theme.of(
+                        context,
+                      ).colorScheme.onSurface.withAlpha((0.5 * 255).toInt()),
+                      activeDotColor: Theme.of(
+                        context,
+                      ).colorScheme.primaryContainer,
+                      expansionFactor: 2,
+                    ),
+                    onDotClicked: (final index) {
+                      _pageController.animateToPage(
+                        index,
+                        duration: const Duration(milliseconds: 350),
+                        curve: Curves.easeInOut,
+                      );
+                    },
+                  ),
+                  IconButtonFilledTonalComponent(
+                    icon: Icons.arrow_forward_ios_sharp,
+                    onPressed: _navigateToHome,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
