cccd74e4ae85f6e087e8032ca7e1f548f3a8330e
MostafaSensei106 <wwwmostafagmer100@gmail.com>
2025-07-15 01:55:50 +0300

    feat(ui): Introduce TextFormFieldComponent and enhance auth forms

--- lib/features/page/register/ui/page/register_page.dart
+++ lib/features/page/register/ui/page/register_page.dart
@@ -1,10 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:takyeebbak/core/routing/routes.dart' show Routes;
+import 'package:takyeebbak/core/widgets/app_bar_component/app_bar.dart';
+import 'package:takyeebbak/core/widgets/button_components/elevated_button_components/elevated_button_component.dart';
+import 'package:takyeebbak/core/widgets/button_components/elevated_button_components/elevated_outline_button_component.dart';
 
 class RegisterPage extends StatelessWidget {
   const RegisterPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Text('Register Page');
+    return Scaffold(
+      appBar: ApAppBar(title: 'Login'),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Text('Let\'s create your account'),
+
+              TextField(),
+              TextField(),
+              TextField(),
+              TextField(),
+              TextField(),
+              TextField(),
+
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButtonComponent(
+                  label: 'Register',
+                  onPressed: () {
+                    Navigator.pushReplacementNamed(context, Routes.mainPage);
+                  },
+                ),
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedOutlineButtonComponent(
+                  label: 'I have an account',
+                  onPressed: () {
+                    Navigator.pushReplacementNamed(context, Routes.loginPage);
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/takyeebbak.dart
+++ lib/takyeebbak.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart' show MaterialApp, ThemeData;
 import 'package:flutter/widgets.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:takyeebbak/l10n/app_localizations.dart' show AppLocalizations;
 import 'core/config/constants/app_constants.dart';
 import 'core/routing/app_router.dart';
@@ -11,41 +12,45 @@ class TakyeebBak extends StatelessWidget {
   final AppRouter appRouter = AppRouter();
 
   @override
-  Widget build(final BuildContext context) => MaterialApp(
-    title: AppConstants.appName,
-    theme: ThemeData.light(),
-    darkTheme: ThemeData.dark(),
-    navigatorKey: AppRouter.navigatorKey,
-    initialRoute: Routes.onboardingPage,
-    onGenerateRoute: appRouter.generateRoute,
-    themeAnimationCurve: Curves.easeInOutCirc,
-    debugShowCheckedModeBanner: false,
-    locale: const Locale('en', 'US'),
-    supportedLocales: const [
-      // English (United States)
-      Locale('en', 'US'),
-      // Arabic (Saudi Arabia)
-      Locale('ar', 'SA'),
-    ],
-    localizationsDelegates: [
-      AppLocalizations.delegate,
-      GlobalCupertinoLocalizations.delegate,
-      GlobalWidgetsLocalizations.delegate,
-      GlobalMaterialLocalizations.delegate,
-    ],
-    localeListResolutionCallback: (locales, supportedLocales) =>
-        locales?.firstWhere(
-          (locale) => supportedLocales.contains(locale),
-          orElse: () => const Locale('ar', 'SA'),
-        ),
-    builder: (context, child) {
-      return SafeArea(
-        top: false,
-        bottom: true,
-        left: false,
-        right: false,
-        child: child!,
-      );
-    },
+  Widget build(final BuildContext context) => ScreenUtilInit(
+    designSize: const Size(375, 812),
+    splitScreenMode: true,
+    child: MaterialApp(
+      title: AppConstants.appName,
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      navigatorKey: AppRouter.navigatorKey,
+      initialRoute: Routes.onboardingPage,
+      onGenerateRoute: appRouter.generateRoute,
+      themeAnimationCurve: Curves.easeInOutCirc,
+      debugShowCheckedModeBanner: false,
+      locale: const Locale('en', 'US'),
+      supportedLocales: const [
+        // English (United States)
+        Locale('en', 'US'),
+        // Arabic (Saudi Arabia)
+        Locale('ar', 'SA'),
+      ],
+      localizationsDelegates: [
+        AppLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+      ],
+      localeListResolutionCallback: (locales, supportedLocales) =>
+          locales?.firstWhere(
+            (locale) => supportedLocales.contains(locale),
+            orElse: () => const Locale('ar', 'SA'),
+          ),
+      builder: (context, child) {
+        return SafeArea(
+          top: false,
+          bottom: true,
+          left: false,
+          right: false,
+          child: child!,
+        );
+      },
+    ),
   );
 }
