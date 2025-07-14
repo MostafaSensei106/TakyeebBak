5a6226bebdaba79a97e3b91416f72b7df3e45194
MostafaSensei106 <wwwmostafagmer100@gmail.com>
2025-07-14 19:48:38 +0300

    refactor(onboarding): Structure page and enable page view navigation

--- lib/features/page/onboarding_page/page/onboarding_page.dart
+++ lib/features/page/onboarding_page/page/onboarding_page.dart
@@ -2,11 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'package:takyeebbak/core/routing/routes.dart';
-import 'package:takyeebbak/core/widgets/button_components/elevated_button_components/elevated_button_icon_component.dart';
-import 'package:takyeebbak/core/widgets/button_components/icon_button_components/icon_button_component.dart';
 import 'package:takyeebbak/core/widgets/button_components/icon_button_components/icon_button_filled_component.dart';
-import 'package:takyeebbak/core/widgets/button_components/icon_button_components/icon_button_filledtonal_component.dart';
-import 'package:takyeebbak/core/widgets/lottie_component/lottie_component.dart';
+
 
 class OnboardingPage extends StatefulWidget {
   const OnboardingPage({super.key});
@@ -45,67 +42,75 @@ class _OnboardingPageState extends State<OnboardingPage> {
 
   @override
   Widget build(BuildContext context) {
-    if (_loading) {
-      return const Scaffold(body: Center(child: CircularProgressIndicator()));
-    }
-    return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8.0),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
+  return Scaffold(
+    body: _loading
+        ? const Center(child: CircularProgressIndicator())
+        : SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Column(
                 children: [
-                  TextButton(
-                    onPressed: _navigateToHome,
-                    child: const Text('Skip'),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      TextButton(
+                        onPressed: _navigateToHome,
+                        child: const Text('Skip'),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-              // PageView(
-              //   controller: _pageController,
-              //   children: const [
-              //     LottieComponent(lottiePath: 'assets/lottie/1.json', text: 'Welcome to Takyeebbak!')
-              //   ],
-              // ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  SmoothPageIndicator(
-                    controller: _pageController,
-                    count: 4,
-                    effect: ExpandingDotsEffect(
-                      // dotWidth: AppC.indicatorDotSize,
-                      // dotHeight: SenseiConst.indicatorDotSize,
-                      dotColor: Theme.of(
-                        context,
-                      ).colorScheme.onSurface.withAlpha((0.5 * 255).toInt()),
-                      activeDotColor: Theme.of(
-                        context,
-                      ).colorScheme.primaryContainer,
-                      expansionFactor: 2,
+                  Expanded(
+                    child: PageView(
+                      controller: _pageController,
+                      children: const [
+                        Placeholder(),
+                        Placeholder(),
+                        Placeholder(),
+                        Placeholder(),
+                      ],
                     ),
-                    onDotClicked: (final index) {
-                      _pageController.animateToPage(
-                        index,
-                        duration: const Duration(milliseconds: 350),
-                        curve: Curves.easeInOut,
-                      );
-                    },
                   ),
-                  IconButtonFilledComponent(
-                    icon: Icons.arrow_forward_ios_sharp,
-                    onPressed: _navigateToHome,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      SmoothPageIndicator(
+                        controller: _pageController,
+                        count: 4,
+                        effect: ExpandingDotsEffect(
+                          dotColor: Theme.of(context)
+                              .colorScheme
+                              .onSurface
+                              .withAlpha((0.5 * 255).toInt()),
+                          activeDotColor: Theme.of(context)
+                              .colorScheme
+                              .primaryContainer,
+                          expansionFactor: 2,
+                        ),
+                        onDotClicked: (index) {
+                          _pageController.animateToPage(
+                            index,
+                            duration: const Duration(milliseconds: 350),
+                            curve: Curves.easeInOut,
+                          );
+                        },
+                      ),
+                      IconButtonFilledComponent(
+                        icon: Icons.arrow_forward_ios_sharp,
+                        onPressed: () {
+                          _pageController.nextPage(
+                            duration: const Duration(milliseconds: 250),
+                            curve: Curves.easeInOut,
+                          );
+                        },
+                      ),
+                    ],
                   ),
                 ],
               ),
-            ],
+            ),
           ),
-        ),
-      ),
-    );
-  }
+  );
+}
 
   @override
   void dispose() {
